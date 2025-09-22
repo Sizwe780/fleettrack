bfc390d846074d956dfa8824c8820d0a7158d982
Sizwe Ngwenya <sizwe.ngwenya78@gmail.com>
2025-09-23 01:40:09 +0200

    Major upgrades, FleetTrack v1.6 Finally ready

--- client/src/utils/routeOptimizer.js
+++ client/src/utils/routeOptimizer.js
@@ -1,10 +1,8 @@
 // src/utils/routeOptimizer.js
 
 const routeOptimizer = (trips) => {
-    // This is a placeholder for your route optimization logic.
-    // You can implement the actual algorithm here later.
-    console.log('Optimizing routes for:', trips);
-    return trips; // Return the trips for now
+    // Add your route optimization logic here later.
+    return trips; 
   };
   
   export default routeOptimizer;
