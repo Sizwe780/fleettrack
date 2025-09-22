ab7a56072d47162d4ecf36315ba069fc673c92c9
Sizwe Ngwenya <sizwe.ngwenya78@gmail.com>
2025-09-22 05:10:39 +0200

    Updated code files

--- client/src/firebase.js
+++ client/src/firebase.js
@@ -4,17 +4,16 @@ import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAla1ZaxyeLc9WBDvKbAS8I9hUZnxIWxPg",
-  authDomain: "fleettrack-84eb6.firebaseapp.com",
-  projectId: "fleettrack-84eb6",
-  storageBucket: "fleettrack-84eb6.appspot.com", // ✅ corrected
-  messagingSenderId: "918797565578",
-  appId: "1:918797565578:web:8c8ee2b227057e21cbf773",
-  measurementId: "G-EQT2H5SQ4V"
-};
-
+    apiKey: "AIzaSyAla1ZaxyeLc9WBDvKbAS8I9hUZnxIWxPg",
+    authDomain: "fleettrack-84eb6.firebaseapp.com",
+    projectId: "fleettrack-84eb6",
+    storageBucket: "fleettrack-84eb6.appspot.com", // ✅ fixed domain
+    messagingSenderId: "918797565578",
+    appId: "1:918797565578:web:8c8ee2b227057e21cbf773",
+    measurementId: "G-EQT2H5SQ4V"
+  };
+  
 const app = initializeApp(firebaseConfig);
-
-export const auth = getAuth(app);
+const auth = getAuth(app);
 export const db = getFirestore(app);
 export const storage = getStorage(app);
