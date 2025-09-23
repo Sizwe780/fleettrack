855bede6fee528fe421b7edfee8f2b54bf95f16e
Sizwe Ngwenya <sizwe.ngwenya78@gmail.com>
2025-09-23 04:01:02 +0200

    Major upgrades, FleetTrack v1.6 Finally ready

---
+++ client/public/firebase-messaging-sw.js
@@ -0,0 +1,12 @@
+// public/firebase-messaging-sw.js
+importScripts('https://www.gstatic.com/firebasejs/10.3.1/firebase-app-compat.js');
+importScripts('https://www.gstatic.com/firebasejs/10.3.1/firebase-messaging-compat.js');
+
+firebase.initializeApp({
+  apiKey: "YOUR_API_KEY",
+  projectId: "YOUR_PROJECT_ID",
+  messagingSenderId: "YOUR_SENDER_ID",
+  appId: "YOUR_APP_ID"
+});
+
+const messaging = firebase.messaging();
